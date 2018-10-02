49b60f3b999e968932f03b5a489a90df40846ef9
Andre <andre.censitario@ibge.gov.br>
2018-10-02 16:19:26 -0300

    added support to projected fields

--- bcim/serializers.py
+++ bcim/serializers.py
@@ -6,6 +6,20 @@ from rest_framework.reverse import reverse
 from .models import *
 
 class UnidadeFederacaoSerializer(GeoFeatureModelSerializer):
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
+
+        # Instantiate the superclass normally
+        super(UnidadeFederacaoSerializer, self).__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
     class Meta:
         model = UnidadeFederacao
         geo_field = 'geom'
